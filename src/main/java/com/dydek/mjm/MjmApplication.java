027ba016756b4553cca0d3717028057da2a05bbd
Przemyslaw Delimata <delimata.ps@gmail.com>
2023-05-25 16:23:12 +0200

    Initial commit

---
+++ src/main/java/com/dydek/mjm/MjmApplication.java
@@ -0,0 +1,13 @@
+package com.dydek.mjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dydek/mjm/MjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dydek.mjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
